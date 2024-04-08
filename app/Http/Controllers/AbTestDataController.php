b20970b8ce8d0a93ca564a78688fda4fed74778e
Kacper Cömlek <kacper74@hotmail.com>
2024-04-08 15:04:11 +0200

    Added Demo testData Page

---
+++ app/Http/Controllers/AbTestDataController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AbTestData;
+use Illuminate\Http\Request;
+
+class AbTestDataController extends Controller {
+    public function index() {
+        return AbTestData::all();
+    }
+
+    public function store(Request $request) {
+        $data = $request->validate(['ab_testname' => ['required'],]);
+
+        return AbTestData::create($data);
+    }
+
+    public function show(AbTestData $abTestData) {
+        return $abTestData;
+    }
+
+    public function update(Request $request, AbTestData $abTestData) {
+        $data = $request->validate(['ab_testname' => ['required'],]);
+
+        $abTestData->update($data);
+
+        return $abTestData;
+    }
+
+    public function destroy(AbTestData $abTestData) {
+        $abTestData->delete();
+
+        return response()->json();
+    }
+}
---
+++ app/Models/AbTestData.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AbTestData extends Model {
+    protected $table = 'ab_testdata';
+}
