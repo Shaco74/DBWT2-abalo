c716910abb4d0f95ddba7446dce23c1b0f98630e
Kacper Cömlek <kacper74@hotmail.com>
2024-04-15 10:45:10 +0200

    adjusted composer import resolver

--- database/seeders/DevelopmentData.php
+++ database/seeders/DevelopmentData.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Util\TimeUtil;
 
 /**
  * Class DevelopmentData
