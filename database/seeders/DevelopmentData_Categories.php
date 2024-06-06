a55aba8747e86c3c022387e6b11f35fa2da15b04
Amier Shraideh <amiershr@gmail.com>
2024-06-06 17:01:40 +0200

    P4: Task 5
    Added a seeder for Category
    
    php artisan db:seed --class=DevelopmentData_Categories

---
+++ database/seeders/DevelopmentData_Categories.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use SplFileObject;
+
+class DevelopmentData_Categories extends Seeder
+{
+    public function run()
+    {
+        $file = new SplFileObject(storage_path('seeding/development/article_has_articlecategory.csv'));
+        $file->setCsvControl(';');
+        $file->setFlags(SplFileObject::READ_CSV);
+
+
+        foreach ($file as $row) {
+            DB::table('ab_article_has_articlecategory')->insert([
+                'ab_articlecategory_id' => intval($row[0]),
+                'ab_article_id' => intval($row[1]),
+            ]);
+        }
+    }
+}
