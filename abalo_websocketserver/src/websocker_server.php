512f64b72aca925854e3c6b8d8575d2ea1416e1a
Kacper <kacper74@hotmail.com>
2024-06-27 12:29:25 +0200

    added Websocket project

---
+++ abalo_websocketserver/src/websocker_server.php
@@ -0,0 +1,43 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+class Chat implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        echo "New message: {$msg}\n";
+        foreach ($from->WebSocket->connections as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8085
+);
+
+$server->run();
